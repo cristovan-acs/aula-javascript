e073a0b14c9fb8eb291d619c91d58e81cbedfb2f
cristovan-acs <cristovan.acs@gmail.com>
2020-06-09 22:44:36 -0300

    Meu primeiro commit JavaScritp

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var nome = "Cristovan Silva";
+var idade = 44;
+var idade2 = 43;
+var frase = "Japão é o melhor time do mundo";
+//alert(nome + " tem " + idade + " anos")
+//alert(idade + idade2)
+console.log(nome);
+console.log(idade + idade2);
+//console.log(frase.replace("Japão", "Brasil"));
+//alert(frase.replace("Japão", "Brasil"));
+console.log(frase.toUpperCase());
